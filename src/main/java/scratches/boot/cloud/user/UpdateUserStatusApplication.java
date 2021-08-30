a45fdfb1068311d80a16f009b67a086a4f5af96f
Rashidi Zin <rashidi@zin.my>
2021-08-30 11:38:14 +0800

    Initial commit

---
+++ src/main/java/scratches/boot/cloud/user/UpdateUserStatusApplication.java
@@ -0,0 +1,13 @@
+package scratches.boot.cloud.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpdateUserStatusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UpdateUserStatusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/scratches/boot/cloud/user/UpdateUserStatusApplicationTests.java
@@ -0,0 +1,13 @@
+package scratches.boot.cloud.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpdateUserStatusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
